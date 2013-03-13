b6a591a6ce0b4eb3fdac4ce3a50b9345b42a771f
Matthew Wang <mattwyl@gmail.com>
2013-03-13 21:54:38 +0800

    Version bump up to 0.8

--- cdiff.py
+++ cdiff.py
@@ -8,7 +8,7 @@ python (>= 2.5.0) and ``less``.
 """
 
 META_INFO = {
-    'version'     : '0.7.1',
+    'version'     : '0.8',
     'license'     : 'BSD-3',
     'author'      : 'Matthew Wang',
     'email'       : 'mattwyl(@)gmail(.)com',
