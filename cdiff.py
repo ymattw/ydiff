e19f384ba4efd8b5af60331ca26efa1effa8154f
Matthew Wang <mattwyl@gmail.com>
2013-05-20 13:44:29 +0800

    Fix issue #23 with --no-ext-diff
    Use `--no-ext-diff` to disable GIT_EXTERNAL_DIFF and diff.external which might
    break cdiff output

--- cdiff.py
+++ cdiff.py
@@ -8,7 +8,7 @@ python (>= 2.5.0) and ``less``.
 """
 
 META_INFO = {
-    'version'     : '0.9',
+    'version'     : '0.9.1',
     'license'     : 'BSD-3',
     'author'      : 'Matthew Wang',
     'email'       : 'mattwyl(@)gmail(.)com',
@@ -54,7 +54,7 @@ COLORS = {
 VCS_INFO = {
     'Git': {
         'probe' : ['git', 'rev-parse'],
-        'diff'  : ['env', '-u', 'GIT_EXTERNAL_DIFF', 'git', 'diff'],
+        'diff'  : ['git', 'diff', '--no-ext-diff'],
         'log'   : ['git', 'log', '--patch'],
     },
     'Mercurial': {
