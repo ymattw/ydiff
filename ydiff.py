7e1d49854b32dc14893e5783c5036b8f5246a9bc
Matt Wang <ymattw@users.noreply.github.com>
2024-11-10 18:16:19 +0000

    Remove unused colors

--- ydiff.py
+++ ydiff.py
@@ -35,19 +35,13 @@ if sys.hexversion < 0x03000000:
 
 class Color(object):
     RESET = '\x1b[0m'
-    UNDERLINE = '\x1b[4m'
     REVERSE = '\x1b[7m'
     # https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
     FG_RED = '\x1b[31m'
     FG_GREEN = '\x1b[32m'
     FG_YELLOW = '\x1b[33m'
     FG_BLUE = '\x1b[34m'
-    FG_MAGENTA = '\x1b[35m'
     FG_CYAN = '\x1b[36m'
-    FG_BRIGHT_RED = '\x1b[91m'
-    FG_BRIGHT_GREEN = '\x1b[92m'
-    FG_BRIGHT_YELLOW = '\x1b[93m'
-    FG_BRIGHT_BLUE = '\x1b[94m'
     FG_BRIGHT_MAGENTA = '\x1b[95m'
     FG_BRIGHT_CYAN = '\x1b[96m'
     # https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
